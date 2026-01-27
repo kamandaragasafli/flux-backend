9168d96e4ae6fa0680f6488b920e6c70ba541101
Kamandar <kamandaragasafli@users.noreply.github.com>
2026-01-27 16:21:42 +0400

    flux 0.2

--- tracker/settings.py
+++ tracker/settings.py
@@ -23,6 +23,13 @@ CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:8000",
 ]
 
+CSRF_COOKIE_SECURE = False
+SESSION_COOKIE_SECURE = False
+
+
+CSRF_COOKIE_SAMESITE = 'Lax'
+SESSION_COOKIE_SAMESITE = 'Lax'
+
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
